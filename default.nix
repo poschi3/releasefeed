c874d46d3b7474af242f41cc9c39be90d8fd7dbc
Poschi <development@poschi.net>
2025-09-21 23:46:33 +0200

    Prepare version 0.0.4-snapshot

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let pkgs = import <nixpkgs> { };
 in pkgs.buildGoModule rec {
   pname = "releasefeed";
-  version = "0.0.3";
+  version = "0.0.4-snapshot";
   src = pkgs.lib.cleanSource ./.;
   vendorHash = "sha256-/OzNsgU3VNnkL9sXDoZahJ7fMqoYCEmstnNnGvmF03A=";
 
