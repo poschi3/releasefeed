892bae5197ff5730c7c5f5a99347240f1c81c5d4
Poschi <development@poschi.net>
2025-09-22 15:42:10 +0200

    Release 0.0.6

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let pkgs = import <nixpkgs> { };
 in pkgs.buildGoModule rec {
   pname = "releasefeed";
-  version = "0.0.6-snapshot";
+  version = "0.0.6";
   src = pkgs.lib.cleanSource ./.;
   vendorHash = "sha256-wroOJEkMNJpKf9OH16a4RqY8NThy6sOTt+TrdRtjpr8=";
 
