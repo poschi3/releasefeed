2dac67595e61fe6bcd027ed139e25d82833676ca
Poschi <development@poschi.net>
2025-09-22 15:41:48 +0200

    Fixed nix vendorHash

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ in pkgs.buildGoModule rec {
   pname = "releasefeed";
   version = "0.0.6-snapshot";
   src = pkgs.lib.cleanSource ./.;
-  vendorHash = "sha256-Er+A+yACCFuSPsm+mQzQFpbUCKNu0khX724lpBHIi4Q=";
+  vendorHash = "sha256-wroOJEkMNJpKf9OH16a4RqY8NThy6sOTt+TrdRtjpr8=";
 
   # network access is required for tests
   doCheck = false;
